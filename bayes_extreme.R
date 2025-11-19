18b344cb64708fa1ff310de94d56a0651a3474a8
Giwrgaras <karatzasgeorge998@gmail.com>
2025-11-19 11:49:33 +0200

    added 2 plots instead of 1

--- bayes_extreme.R
+++ bayes_extreme.R
@@ -1,4 +1,4 @@
-T <- 6 # number of trials
+T <- 60 # number of trials
 theta <- 0.7 # true parameter value
 y <- rbinom(T, size = 1, prob = theta) # simulate data
 ST <- sum(y) # number of successes
@@ -15,25 +15,29 @@ post_vals <- post_vals / sum(post_vals) # normalize posterior using sum instead 
 
 mle <- ST / T # maximum likelihood estimate for plotting purposes
 
-
-
 # Plot prior and posterior
+par(mfrow = c(2, 1), mar = c(4, 4, 3, 1))
+
+# Plot prior
+plot(x, prior_vals,
+     type = "l",
+     col = "red",
+     lwd = 2,
+     main = "Triangular Prior",
+     xlab = expression(theta),
+     ylab = "Density")
+
+# Plot posterior
 plot(x, post_vals,
      type = "l",
      col = "blue",
      lwd = 3,
-     ylim = c(0, max(post_vals)),
-     main = "Triangular Prior and Posterior",
+     main = "Posterior",
      xlab = expression(theta),
      ylab = "Density")
-
-lines(x, prior_vals / max(prior_vals) * max(post_vals),
-      col = "black", lty = 2, lwd = 2)
-
 abline(v = mle, col = "red", lwd = 2, lty = 3)
-
 legend("topleft",
-       legend = c("Posterior", "Prior (scaled)", "MLE"),
-       col = c("blue", "black", "red"),
-       lty = c(1, 2, 3),
-       lwd = c(3, 2, 2))
+       legend = c("Posterior", "MLE"),
+       col = c("blue", "red"),
+       lty = c(1, 3),
+       lwd = c(3, 2))
